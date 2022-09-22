a83a6046e99ddd8fd19e062ec9b13c73f96e5d82
anis ben ali <anisbenali001002@gmail.com>
2022-09-22 01:35:02 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,74 @@
+window.addEventListener("DOMContentLoaded", function(){
+	const stopWatch = document.getElementById("stopWatch");
+	const startBtn = document.getElementById("start");
+	const stopBtn = document.getElementById("stop");
+	const resetBtn = document.getElementById("reset");
+
+	let startTimer = null;
+	let [hours, minutes, seconds] = [0,0,0];
+
+	function disabled(...elems) {
+		elems.forEach((elem) => elem.setAttribute("disabled","true"));
+
+	}
+
+	function unDisabled(...elems){
+		elems.forEach((elem) => elem.removeAttribute("disabled"));
+	}
+
+	function displyTime() {
+		seconds++;
+		if (seconds >= 60) {
+			minutes++;
+			seconds=0;
+
+			if (minutes >= 60) {
+				hours++;
+				minutes = 0;
+
+				if (hours>= 24) {
+					hours = 0;
+				}
+			}
+		}
+
+		let h = hours < 10 ? "0" + hours : hours;
+		let m = minutes < 10 ? "0" + minutes : minutes;
+		let s = seconds < 10 ? "0" + seconds : seconds;
+
+		stopWatch.innerHTML = `${h} : ${m} : ${s}`
+	}
+
+	startBtn.addEventListener("click", () => {
+		if (startTimer !==null){
+			cleanInterval(startTimer);
+
+		}
+
+		startTimer = setInterval(displyTime,1000);
+
+		disabled(strtBtn);
+		unDisabled(stopBtn,resetBtn);
+	});
+
+	stopBtn.addEventListener("click", function(){
+		cleanInterval(startTimer);
+
+		disabled(stopBtn);
+		unDisabled(startBtn);
+	});
+
+	resetBtn.addEventListener("click", function () {
+		cleanInterval(startTimer);
+
+		disabled(resetBtn,stopBtn);
+		unDisabled(startBtn);
+		[hours,minutes,seconds] = [0,0,0];
+
+		stopWatch.innerHTML = "00 : 00 : 00";
+	});
+	
+
+}
+
+	)
